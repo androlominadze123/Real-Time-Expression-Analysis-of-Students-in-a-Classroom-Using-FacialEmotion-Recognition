716898e600fb350e49fbe306f6cc6856ff2e7110
androlominadze123 <andro.lominadze26@gmail.com>
2019-03-27 19:51:16 +0200

    Update object_tracker.py
    repo

--- object_tracker.py
+++ object_tracker.py
@@ -1,4 +1,7 @@
 # import the necessary packages
+!pip install opencv-python
+!apt update && apt install -y libsm6 libxext6
+
 from pyimagesearch.centroidtracker import CentroidTracker
 from imutils.video import VideoStream
 import numpy as np
