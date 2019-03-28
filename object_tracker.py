738db0d56230fcc11ec64234356700d2612349a9
androlominadze123 <andro.lominadze26@gmail.com>
2019-03-28 17:38:23 +0200

    Update object_tracker.py

--- object_tracker.py
+++ object_tracker.py
@@ -1,4 +1,6 @@
-# import the necessary packfrom pyimagesearch.centroidtracker import CentroidTracker
+# import the necessary packages
+
+from pyimagesearch.centroidtracker import CentroidTracker
 from imutils.video import VideoStream
 import numpy as np
 import argparse
